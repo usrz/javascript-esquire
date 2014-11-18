0699da553f07b300a94b5e2ec143705c6ec27eeb
Pier Fumagalli <ianosh@gmail.com>
2014-11-19 02:12:49 +0900

    Window title from "package.json".

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,7 +61,6 @@ module.exports = function(grunt) {
           "plugins": ["plugins/markdown"],
           "templates": {
             "cleverLinks": true,
-            "windowTitle": "Esquire API"
           },
           "markdown": {
             "parser": "gfm",
